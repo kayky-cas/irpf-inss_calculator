b5c2fcb1a06ebc72a36cd1532a38c8c630e3a26f
kayky-cas <57455569+kayky-cas@users.noreply.github.com>
2020-04-27 10:07:30 -0300

    final (logo and title)

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Impostos</title>
+        <title>Calculadora INSS-IRPF</title>
         <link rel="stylesheet" href="./css/main.css">
         <meta name="author" content="Kayky Belleboni Casagrande">
         <meta name="description" content="Aplicativo para calcular descontos referentes a INSS e IRPF">
         <meta name="keywords" content="imposto, inss, irpf">
         <meta name="viewport" content="widht=device-widht,initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <!-- <link rel="icon" href="./img/logo32.png" sizes="32x32"> -->
+        <link rel="icon" href="./img/logos/logo32.png" sizes="32x32">
     </head>
 
     <body>
